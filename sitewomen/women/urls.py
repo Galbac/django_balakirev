8a4048878e9a53c97655fe6c1ab84a6d30ade0dd
Galbac <zidan_2002@mail.ru>
2025-04-14 13:52:42 +0300

    First commit

---
+++ sitewomen/women/urls.py
@@ -0,0 +1,21 @@
+from django.http import HttpRequest, HttpResponseNotFound
+
+from . import converters
+
+from django.urls import path, register_converter
+from . import views
+
+register_converter(converters.FourDigitYearConverter, 'year4')
+
+urlpatterns = [
+    path('', views.index, name='home'),
+    path('about/',views.about, name='about'),
+    path('post/<int:post_id>',views.show_post,name="post"),
+    path('addpage/',views.addpage, name="addpage"),
+    path('contact/',views.contact,name='contact'),
+    path('login/',views.login,name='login')
+]
+
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
---
+++ sitewomen/women/views.py
@@ -0,0 +1,47 @@
+from django.http import HttpResponse, Http404, HttpResponsePermanentRedirect
+from django.shortcuts import render
+from django.urls import reverse
+
+menu = [{'title': "О сайте", 'url_name': 'about'},
+        {'title': "Добавить статью", 'url_name': 'addpage'},
+        {'title': "Обратная связь", 'url_name': 'contact'},
+        {'title': "Войти", 'url_name': 'login'}
+        ]
+
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
+]
+
+
+# Функция, которая проверяет, является ли число простым
+
+
+def index(request):
+    data = {
+        'title': 'Главная страница',
+        'menu': menu,
+        'bios': data_db
+    }
+    return render(request, 'women/index.html', data)
+
+
+def about(request):
+    return render(request, 'women/about.html', {"title": "О сайте","menu":menu})
+
+
+def show_post(request, post_id):
+    return HttpResponse(f'Оторбражение статьи c id = {post_id}')
+
+
+def addpage(request):
+    return HttpResponse("Добавление статьи")
+
+
+def contact(request):
+    return HttpResponse("Обратная связь")
+
+
+def login(request):
+    return HttpResponse("Авторизация")

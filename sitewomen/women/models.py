10311ffccf2b0093233bc5ceb5cac78f90a5497f
Galbac <zidan_2002@mail.ru>
2025-04-22 08:38:02 +0300

    lesson 4.3

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -18,7 +18,7 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
-    cat = models.ForeignKey("Category", on_delete=models.PROTECT)
+    cat = models.ForeignKey("Category", on_delete=models.PROTECT, related_name='posts')
 
     objects = models.Manager()
     published = PublishedModel()
@@ -34,6 +34,7 @@ class Women(models.Model):
 
     def __str__(self):
         return f' {self.title}, {self.is_published}'
+
 
 class Category(models.Model):
     name = models.CharField(max_length=100, db_index=True)

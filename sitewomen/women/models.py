3de3a952946ba80c33073e5622e7b95fdee10148
Galbac <zidan_2002@mail.ru>
2025-04-24 22:50:57 +0300

    lesson 6.3

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models.fields import TextField, CharField
 from django.urls import reverse
 
 
@@ -12,15 +13,17 @@ class Women(models.Model):
         DRAFT = 0, 'Черновик'
         PUBLISHED = 1, 'Опубликовано'
 
-    title = models.CharField(max_length=255)
-    slug = models.SlugField(max_length=255, unique=True, db_index=True)
-    content = models.TextField(blank=True)
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(choices=Status.choices, default=Status.DRAFT)
-    cat = models.ForeignKey("Category", on_delete=models.PROTECT, related_name='posts')
-    tags = models.ManyToManyField('TagPost', blank=True, related_name="tags")
-    husband = models.OneToOneField('Husband', on_delete=models.SET_NULL, null=True, blank=True, related_name='wman')
+    title = models.CharField(max_length=255, verbose_name='Заголовок')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, )
+    content = models.TextField(blank=True, verbose_name='Текст статьи')
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
+    time_update = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
+    is_published = models.BooleanField(choices=tuple(map(lambda x: (bool(x[0]), x[1]), Status.choices)),
+                                       default=Status.DRAFT, verbose_name="Статус")
+    cat = models.ForeignKey("Category", on_delete=models.PROTECT, related_name='posts', verbose_name='Категории')
+    tags = models.ManyToManyField('TagPost', blank=True, related_name="tags", verbose_name='Тэги')
+    husband = models.OneToOneField('Husband', on_delete=models.SET_NULL, null=True, blank=True, related_name='wman',
+                                   verbose_name='Муж')
 
     objects = models.Manager()
     published = PublishedModel()
@@ -41,7 +44,11 @@ class Women(models.Model):
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, db_index=True)
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
+
+    name = models.CharField(max_length=100, db_index=True, verbose_name='Категория')
     slug = models.SlugField(max_length=255, unique=True, db_index=True)
 
     def get_absolute_url(self):
@@ -63,9 +70,9 @@ class TagPost(models.Model):
 
 
 class Husband(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.SlugField(max_length=100)
     age = models.IntegerField(null=True)
-    m_count = models.IntegerField(blank=True,default=0)
+    m_count = models.IntegerField(blank=True, default=0)
 
     def __str__(self):
         return self.name

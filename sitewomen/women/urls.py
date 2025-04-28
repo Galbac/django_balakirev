39def551ef3acfcd25fd2b03c323b54cfd522000
Galbac <zidan_2002@mail.ru>
2025-04-28 16:17:16 +0300

    lesson 8.1

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -8,10 +8,12 @@ from . import views
 register_converter(converters.FourDigitYearConverter, 'year4')
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    # path('', views.index, name='home'),
+    path('', views.WomenHome.as_view(), name='home'),
     path('about/', views.about, name='about'),
     path('post/<slug:post_slug>', views.show_post, name="post"),
-    path('addpage/', views.addpage, name="addpage"),
+    # path('addpage/', views.addpage, name="addpage"),
+    path('addpage/', views.AddPage.as_view(), name="addpage"),
     path('contact/', views.contact, name='contact'),
     path('login/', views.login, name='login'),
     path('category/<slug:cat_slug>/', views.show_category, name='category'),

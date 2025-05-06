3bd8d4d322481e801a7c972aa612bc0a64b89a02
Galbac <zidan_2002@mail.ru>
2025-05-06 14:05:22 +0300

    lesson 9

--- sitewomen/users/views.py
+++ sitewomen/users/views.py
@@ -1,11 +1,12 @@
-from django.contrib.auth import logout
+from django.contrib.auth import logout, get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views import View
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 
-from .forms import LoginUserForm, RegisterUserForm
+from .forms import LoginUserForm, RegisterUserForm, ProfileUserForm
 
 
 class LoginUser(LoginView):
@@ -20,17 +21,6 @@ class LogoutUser(View):
         return redirect('users:login')
 
 
-# def register(request):
-#     if request.method == "POST":
-#         form = RegisterUserForm(request.POST)
-#         if form.is_valid():
-#             user = form.save(commit=False)
-#             user.set_password(form.cleaned_data['password1'])
-#             user.save()
-#             return render(request, 'users/register_done.html')
-#     else:
-#         form = RegisterUserForm()
-#     return render(request, 'users/register.html', {'form': form})
 class RegisterUser(CreateView):
     form_class = RegisterUserForm
     template_name = 'users/register.html'
@@ -40,3 +30,14 @@ class RegisterUser(CreateView):
     success_url = reverse_lazy('users:login')
 
 
+class ProfileUser(LoginRequiredMixin, UpdateView):
+    model = get_user_model()
+    form_class = ProfileUserForm
+    template_name =  'users/profile.html'
+    extra_context = {'title': "Профиль пользователя"}
+
+    def get_success_url(self):
+        return reverse_lazy('users:profile', args=[self.request.user.pk])
+
+    def get_object(self, queryset=None):
+        return self.request.user

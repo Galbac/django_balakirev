3b7e46447115d83b78a3b816518edcf290e3bf15
Galbac <zidan_2002@mail.ru>
2025-05-06 08:17:22 +0300

    lesson 9.7

--- sitewomen/users/views.py
+++ sitewomen/users/views.py
@@ -1,11 +1,11 @@
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout
 from django.contrib.auth.views import LoginView
-from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render
-from django.urls import reverse, reverse_lazy
+from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic import CreateView
 
-from .forms import LoginUserForm
+from .forms import LoginUserForm, RegisterUserForm
 
 
 class LoginUser(LoginView):
@@ -13,25 +13,30 @@ class LoginUser(LoginView):
     template_name = 'users/login.html'
     extra_context = {'title': 'Авторизация'}
 
-    # def get_success_url(self):
-    #     return reverse_lazy('home')
 
-# def login_user(request):
-#     if request.method == 'POST':
-#         form = LoginUserForm(request.POST)
+class LogoutUser(View):
+    def get(self, request):
+        logout(request)
+        return redirect('users:login')
+
+
+# def register(request):
+#     if request.method == "POST":
+#         form = RegisterUserForm(request.POST)
 #         if form.is_valid():
-#             cd = form.cleaned_data
-#             user = authenticate(request, username=cd['username'],
-#                                 password=cd['password'])
-#             if user and user.is_active:
-#                 login(request, user)
-#                 return HttpResponseRedirect(reverse('home'))
+#             user = form.save(commit=False)
+#             user.set_password(form.cleaned_data['password1'])
+#             user.save()
+#             return render(request, 'users/register_done.html')
 #     else:
-#         form = LoginUserForm()
-#
-#     return render(request, 'users/login.html', {'form': form})
+#         form = RegisterUserForm()
+#     return render(request, 'users/register.html', {'form': form})
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'users/register.html'
+    extra_context = {
+        'title': "Регистрация"
+    }
+    success_url = reverse_lazy('users:login')
 
 
-# def logout_user(request):
-#     logout(request)
-#     return HttpResponseRedirect(reverse('users:login'))

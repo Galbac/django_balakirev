d6728dfbce68ac434da25b7233aff00f64e7af8f
Galbac <zidan_2002@mail.ru>
2025-05-07 16:49:50 +0300

    9.14

--- sitewomen/users/forms.py
+++ sitewomen/users/forms.py
@@ -38,7 +38,7 @@ class RegisterUserForm(UserCreationForm):
 
     def clean_email(self):
         email = self.cleaned_data['email']
-        if User.objects.filter(email=email).exists():
+        if get_user_model().objects.filter(email=email).exists():
             raise forms.ValidationError("Такой E-mail уже существует!")
         return email
 
@@ -47,7 +47,8 @@ class ProfileUserForm(forms.ModelForm):
     username = forms.CharField(disabled=True, label='Логин', widget=forms.TextInput(attrs={'class': 'form-input'}))
     email = forms.CharField(disabled=True, label='E-mail', widget=forms.TextInput(attrs={'class': 'form-input'}))
     this_year = datetime.date.today().year
-    date_birth = forms.DateField(label='Дата рождения',widget=forms.SelectDateWidget(years=tuple(range(this_year - 100, this_year - 5))))
+    date_birth = forms.DateField(label='Дата рождения',
+                                 widget=forms.SelectDateWidget(years=tuple(range(this_year - 100, this_year - 5))))
 
     class Meta:
         model = get_user_model()
